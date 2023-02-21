8add22f57e9c44da94cb2c583e79c1a8f1d57cbb
Carla Regina Mendes <carla.regina.mendes@avanade.com>
2023-02-21 12:46:12 -0300

    first commit

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public IEnumerable<Evento> _evento = new Evento[]{
+        new Evento(){
+            EventoId = 1 ,
+            Tema = "Angular 11",
+            Local = "BH",
+            Lote = "1 lote",
+            QtdPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString()
+           },
+        new Evento() {
+            EventoId = 2 ,
+            Tema = "Angular 13",
+            Local = "SP",
+            Lote = "1 lote",
+            QtdPessoas = 150,
+            DataEvento = DateTime.Now.AddDays(2).ToString()
+
+
+           }
+        };
+ 
+
+        
+
+        public EventoController()
+        {
+           
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento>  Get()
+        {
+           return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento>  GetById(int id)
+        {
+           return _evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}
