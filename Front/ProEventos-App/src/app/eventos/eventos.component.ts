be2f739b996c3a1afd5bac44630d12d6beb8d7dd
Carla Regina Mendes <carla.regina.mendes@avanade.com>
2023-02-22 19:22:26 -0300

    added filter

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,6 +9,26 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosfiltrados: any = [];
+  private _filtroLista: string = '';
+
+  public get filtroLista (): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista (value: string){
+    this._filtroLista = value;
+    this.eventosfiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista):this.eventos;
+
+  }
+
+  filtrarEventos (filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      ( evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor)!== -1
+    )
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -17,8 +37,11 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error),
+      response => {
+        this.eventos = response;
+        this.eventosfiltrados = this.eventos;
+      },
+      error => console.log(error)
     );
 
   }
